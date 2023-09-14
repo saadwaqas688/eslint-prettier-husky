295c5b6c80192363381f50c13bf802e08a03d64a
saad waqas <saadwaqas688@gmail.com>
2023-09-14 10:31:20 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
-          Edit <code>src/App.js</code> and save to second commit
+          Edit <code>src/App.js</code> and save to third commit
         </p>
 
         <a
