c350a78bfa662dea87a4db78fb11ae7d84be36ea
saad waqas <saadwaqas688@gmail.com>
2023-09-14 12:00:32 +0500

    a

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
-          Edit <code>src/App.js</code> and save to third commitasdfadsfgjgjhgjgk
+          Edit <code>src/App.js</code> and save to third
         </p>
 
         <a
